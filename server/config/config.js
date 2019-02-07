823380b71468ea23d5ea7c6be304ff36e54ea931
Pilar <reyna.camarillo@uanlmexico.mx>
2019-02-06 18:03:23 -0600

    Creando variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'dev') {
 
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://userAdmin:Alito.2019@ds225375.mlab.com:25375/commic';
+    urlDB = process.env.MONGO_URI;
 }
 
 
